641de06caafe1f5b00a5ef9739130d0e45a541d6
daniriolo <dsernag1300@alumno.ipn.mx>
2022-05-22 18:27:46 -0500

    port 4000 commit heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app=express();
 const mongoose = require('mongoose');
 require('dotenv').config()
-const port= process.env.PORT;
+const port= process.env.PORT||4000;
 const studentRoutes = require('./routes/students');
 
 //middlewares
